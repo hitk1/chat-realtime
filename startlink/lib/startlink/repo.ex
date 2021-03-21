464d9b2e4e35524b9155b5f1039e12c88399613c
hitk1 <luispaulo.degini@gmail.com>
2021-03-21 17:38:51 -0300

    refactor: Remoção do projeto elixir com Phoenix

--- startlink/lib/startlink/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Startlink.Repo do
-  use Ecto.Repo,
-    otp_app: :startlink,
-    adapter: Ecto.Adapters.Postgres
-end
--- startlink/lib/startlink_web/views/friend_view.ex
+++
@@ -1,8 +0,0 @@
-defmodule StartlinkWeb.FriendView do
-  alias Startlink.Friends.FriendsModel, as: Relationship
-
-  def render("relatioship_created.json", %{message: message}) do
-    %{ message: message }
-  end
-
-end
